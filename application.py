f33e9cb89303f4166efde0bb780859611ed85bc3
Ezequiel Becke <ezequiel.becke@gmail.com>
2018-03-27 05:48:06 -0400

    changes

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 #importaciones de funciones para ayudarnos
-from cs50 import SQL
+from library50 import cs50
+import os
+import sqlalchemy
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
@@ -354,4 +356,32 @@ def sell():
 
 
     return render_template("sell.html", index= positions, totalPrice= totalPrice)
+
+
+
+
+    class SQL(object):
+    def __init__(self, url):
+        try:
+            self.engine = sqlalchemy.create_engine(url)
+        except Exception as e:
+            raise RuntimeError(e)
+    def execute(self, text, *multiparams, **params):
+        try:
+            statement = sqlalchemy.text(text).bindparams(*multiparams, **params)
+            result = self.engine.execute(str(statement.compile(compile_kwargs={"literal_binds": True})))
+            # SELECT
+            if result.returns_rows:
+                rows = result.fetchall()
+                return [dict(row) for row in rows]
+            # INSERT
+            elif result.lastrowid is not None:
+                return result.lastrowid
+            # DELETE, UPDATE
+            else:
+                return result.rowcount
+        except sqlalchemy.exc.IntegrityError:
+            return None
+        except Exception as e:
+            raise RuntimeError(e)
 
